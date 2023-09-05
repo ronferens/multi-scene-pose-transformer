dea5cb9564ef85b49d61e80444bbfdb07a3c0d0e
ronferens <ronferens@gmail.com>
2023-09-05 16:14:35 +0300

    Added the name of model's folder to the returned values

--- models/pose_regressors.py
+++ models/pose_regressors.py
@@ -1,20 +1,22 @@
 from .posenet.PoseNet import PoseNet
 from .transposenet.EMSTransPoseNet import EMSTransPoseNet
 from  .transposenet.MSTransPoseNet import MSTransPoseNet
+from typing import Dict, Tuple
 
-def get_model(model_name, backbone_path, config):
+
+def get_model(model_name: str, backbone_path: str, config: Dict) -> Tuple:
     """
     Get the instance of the request model
     :param model_name: (str) model name
     :param backbone_path: (str) path to a .pth backbone
     :param config: (dict) config file
-    :return: instance of the model (nn.Module)
+    :return: instance of the model (nn.Module) and the name of the model's folder
     """
     if model_name == 'posenet':
-        return PoseNet(backbone_path)
+        return PoseNet(backbone_path), 'posenet'
     elif model_name == 'ms-transposenet':
-        return MSTransPoseNet(config, backbone_path)
+        return MSTransPoseNet(config, backbone_path), 'transposenet'
     elif model_name == 'ems-transposenet':
-        return EMSTransPoseNet(config, backbone_path)
+        return EMSTransPoseNet(config, backbone_path), 'transposenet'
     else:
         raise "{} not supported".format(model_name)
